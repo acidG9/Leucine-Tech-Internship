4ecb682fc17463a70204721ef6c6503576664ceb
Akshansh Vaishnav <akshanshvaishnavpaypal@gmail.com>
2025-05-22 23:25:55 +0530

    link corrected

--- frontend/src/assets/api.js
+++ frontend/src/assets/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://leucine-tech-internship-0fwc.onrender.com' });
+const API = axios.create({ baseURL: 'https://leucine-tech-internship-0fwc.onrender.com/api' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
