39ef94a802c301535303ea159499572b881216ea
Akshansh Vaishnav <akshanshvaishnavpaypal@gmail.com>
2025-05-22 22:48:59 +0530

    deployement links

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ import requestRoutes from './routes/request.routes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://accessmanagementsystem.vercel.app',
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
